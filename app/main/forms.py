c78c04029e0b6aa0d66c10ce6ef3fa591eafd1dc
ilya <ilya.vaitehovich@mail.ru>
2022-08-20 15:54:55 +0300

    Create comment form

--- app/main/forms.py
+++ app/main/forms.py
@@ -3,10 +3,11 @@ from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth import get_user_model
 from django.forms import TextInput, Textarea
-from .models import UserProfile, Book
+from .models import UserProfile, Book, Comment
 
 attrs = {
-    'class': 'from-control'
+    'class': 'from-control',
+    'placeholder': ['First name', 'Last name']
 }
 
 
@@ -14,21 +15,38 @@ class UserProfileEdit(ModelForm):
     class Meta:
         model = UserProfile
         fields = ['avatar', 'first_name', 'last_name', 'age', 'status', 'country', 'city', 'degree', 'organization']
-        widgets = {
-            'first_name': TextInput(attrs={'class': attrs['class'], 'placeholder': 'First name'}),
-            'last_name': TextInput(attrs={'class': attrs['class'], 'placeholder': 'Last name'}),
-            'status': TextInput(attrs={'class': attrs['class'], 'placeholder': 'Your status'}),
-            'country': TextInput(attrs={'class': attrs['class'], 'placeholder': 'Country'}),
-            'city': TextInput(attrs={'class': attrs['class'], 'placeholder': 'City'}),
-            'organization': TextInput(attrs={'class': attrs['class'], 'placeholder': 'Organization'}),
-            'degree': TextInput(attrs={'class': attrs['class'], 'placeholder': 'Your degree'})
-        }
+        widgets = {x: TextInput(attrs={'class': attrs['class'], 'placeholder': x.title()}) for x in fields
+                   if x != 'avatar' and x != 'age'}
+        widgets.update({
+            'first_name': TextInput(attrs={'class': attrs['class'], 'placeholder': attrs['placeholder'][0]}),
+            'last_name': TextInput(attrs={'class': attrs['class'], 'placeholder': attrs['placeholder'][1]})
+        })
+
+        labels = {x: x.title() for x in fields}
+        labels.update({'first_name': attrs['placeholder'][0],
+                       'last_name': attrs['placeholder'][1]})
 
 
 class CreateBookForm(forms.Form):
-    title = forms.CharField(label='Title', max_length=50, widget=TextInput(attrs={'class': attrs['class'], 'placeholder': 'Title'}))
-    genre = forms.CharField(label='Genre', max_length=50, widget=TextInput(attrs={'class': attrs['class'], 'placeholder': 'Genre'}))
-    subtitle = forms.CharField(label='Subtitle', max_length=100,widget=TextInput(attrs={'class': attrs['class'], 'placeholder': 'Subtitle'}))
-    description = forms.CharField(label='Description', widget=Textarea(attrs={'class': attrs['class'], 'placeholder': 'Description'}))
+    title = forms.CharField(label='Title', max_length=50,
+                            widget=TextInput(attrs={'class': attrs['class'], 'placeholder': 'Title'}))
+    genre = forms.CharField(label='Genre', max_length=50,
+                            widget=TextInput(attrs={'class': attrs['class'], 'placeholder': 'Genre'}))
+    subtitle = forms.CharField(label='Subtitle', max_length=100,
+                               widget=TextInput(attrs={'class': attrs['class'], 'placeholder': 'Subtitle'}))
+    description = forms.CharField(label='Description',
+                                  widget=Textarea(attrs={'class': attrs['class'], 'placeholder': 'Description'}))
     file = forms.FileField(label='File')
     cover = forms.ImageField(label='Cover', required=False)
+
+
+class CommentForm(ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['comment']
+        widgets = {
+            'comment': Textarea(attrs={'class': attrs['class'], 'placeholder': 'Comment'})
+        }
+        labels = {
+            'comment': 'Add comment'
+        }
