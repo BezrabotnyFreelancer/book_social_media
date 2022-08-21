3c58bd76d7120a905907a56036031bc08ed7227e
ilya <ilya.vaitehovich@mail.ru>
2022-08-21 17:22:04 +0300

    Add chat urls

--- app/app/urls.py
+++ app/app/urls.py
@@ -21,5 +21,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('', include('main.urls'))
+    path('', include('main.urls')),
+    path('chat/', include('chat.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
