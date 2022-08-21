a47c15600a2a31aae492c73e9ba88c02247ba19a
ilya <ilya.vaitehovich@mail.ru>
2022-08-21 17:22:32 +0300

    Add new function

--- app/main/profile_methods.py
+++ app/main/profile_methods.py
@@ -3,7 +3,13 @@ from .models import UserProfile
 from django.shortcuts import get_object_or_404
 
 
-def get_profile(request):
+def get_main_profile(request):
     user = get_user_model().objects.filter(username__icontains=request.user.get_username()).values('id')
+    profile = get_object_or_404(UserProfile, user=user[0]['id'])
+    return profile
+
+
+def get_another_profile(id):
+    user = get_user_model().objects.filter(id=id).values('id')
     profile = get_object_or_404(UserProfile, user=user[0]['id'])
     return profile
