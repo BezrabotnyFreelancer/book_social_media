122ea95894e0bd9efcdec4eefb364015a8d18eca
BezrabotnyFreelancer <ilya.vaitehovich@mail.ru>
2022-08-25 17:49:29 +0300

    create models

--- app/chat/models.py
+++ app/chat/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+from main.models import UserProfile
+
+
+class Chat(models.Model):
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='sender_user')
+    recipient = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='recipient_user')
+    has_unread = models.BooleanField(default=False)
+    
+    class Meta:
+        verbose_name = 'Chat'
+        verbose_name_plural = 'Chats'
+        ordering = ['-user']
+        
+    def __str__(self):
+        return f'{self.user} - {self.recipient}'
+    
+    
+class Message(models.Model):
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
+    sender_user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='sender')
+    recipient_user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='recipient')
+    body = models.TextField()
+    files = models.FileField(upload_to='message_content/', null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
+    
+    class Meta:
+        verbose_name = 'Message'
+        verbose_name_plural = 'Messages'
+        ordering = ['-chat']
+        
+    def __str__(self):
+        return f'{self.chat} {self.date}'
+    
+    
