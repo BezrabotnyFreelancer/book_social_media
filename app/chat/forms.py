c3a817dca3f73f5e5ae891a1eb813c28c97d5f49
BezrabotnyFreelancer <ilya.vaitehovich@mail.ru>
2022-08-25 17:48:27 +0300

    create chat creation form and message creation form

--- app/chat/forms.py
+++ app/chat/forms.py
@@ -1,3 +1,18 @@
-from django.forms import ModelForm
 from django import forms
-from django.forms import Textarea
+from django.forms import Textarea, TextInput
+
+
+class ChatForm(forms.Form):
+    username = forms.CharField(label='Username', max_length=16, widget=TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Input an username'
+    }))
+    
+    
+class MessageForm(forms.Form):
+    body = forms.CharField(label='Message', widget=Textarea(attrs={
+        'class': 'form-control',
+        'placeholder': 'Write a message ...'
+    }))
+    
+    files = forms.FileField(required=False)
