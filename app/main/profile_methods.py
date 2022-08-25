66b4c48391e1b71c074d76e70c617ba3aed0e364
BezrabotnyFreelancer <ilya.vaitehovich@mail.ru>
2022-08-25 17:50:28 +0300

    add get_profile function

--- app/main/profile_methods.py
+++ app/main/profile_methods.py
@@ -7,3 +7,8 @@ def get_main_profile(request):
     user = get_user_model().objects.filter(username__icontains=request.user.get_username()).values('id')
     profile = get_object_or_404(UserProfile, user=user[0]['id'])
     return profile
+
+def get_profile(username):
+    user = get_user_model().objects.filter(username__icontains=username).values('id')
+    profile = get_object_or_404(UserProfile, user=user[0]['id'])
+    return profile
