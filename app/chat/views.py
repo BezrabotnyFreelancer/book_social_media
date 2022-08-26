1b148fb2eda0ab63ba43b8192fc54224acbac904
BezrabotnyFreelancer <ilya.vaitehovich@mail.ru>
2022-08-26 16:49:19 +0300

    create comtroller-functions

--- app/chat/views.py
+++ app/chat/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render, redirect
-from django.urls import reverse_lazy
-from django.views import View
 from django.db.models import Q
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 from .forms import ChatForm, MessageForm
@@ -10,42 +9,36 @@ from .models import Chat, Message
 from main.profile_methods import get_profile, get_main_profile
 
 
-class CreateChat(LoginRequiredMixin, View):
+@login_required
+def create_chat(request):
+    sender = get_main_profile(request)
     
-    login_url = reverse_lazy('login')
+    if request.method == 'POST':
+        chat_form = ChatForm(request.POST)
+        
+        if chat_form.is_valid():
+            recipient = get_profile(chat_form.cleaned_data['username'])
+            
+            if Chat.objects.filter(user=get_main_profile(request), recipient=recipient).exists():
+                chat = Chat.objects.get(user=get_main_profile(request), recipient=recipient)
+                return redirect('chat', pk=chat.pk)
+            
+            else:
+                chat = Chat()
+                chat.user = sender
+                chat.recipient = recipient
+                chat.save()
+                return redirect('chat', pk=chat.pk)
+            
+        else:
+            context = {'form': chat_form}
+            return render(request, 'chat/create_chat.html', context)
     
-    def get(self, request, *args, **kwargs):
-        form = ChatForm()
-        context = {
-            'form': form
-        }
-        
-        return render(request, 'chat/create_chat.html', context=context)
+    else:
+        chat_form = ChatForm()
+        context = {'form': chat_form}
+        return render(request, 'chat/create_chat.html', context)
     
-    def post(self, request, *args, **kwargs):
-        form = ChatForm(request.POST)
-        
-        if form.is_valid():
-            username = form.cleaned_data['username']
-        
-            try:
-                recipient = get_profile(username)
-                
-                if Chat.objects.filter(user=request.user, recepient=recipient).exists():
-                    chat = Chat.objects.filter(user=get_main_profile(request), recipient=recipient)[0]
-                    return redirect('chat', pk=chat.pk)
-                
-                if form.is_valid():
-                    sender_chat = Chat(
-                        user=get_main_profile(request),
-                        recipient=recipient
-                    )
-                    sender_chat.save()
-                    chat_pk = sender_chat.pk
-                    return redirect('chat', pk=chat_pk)
-            except:
-                return redirect('create_chat')
-
 
 @login_required
 def list_chats(request):
@@ -58,49 +51,48 @@ def list_chats(request):
     
     return render(request, 'chat/inbox.html', context)
 
-
-class CreateMessage(LoginRequiredMixin, View):
-    login_url = reverse_lazy('login')
+@login_required
+def create_message(request, pk):
+    chat = Chat.objects.get(pk=pk)
     
-    def post(self, request, pk, *args, **kwargs):
-        chat = Chat.objects.get(pk=pk)
+    if chat.recipient == get_main_profile(request):
+        recipient = chat.user
         
-        if chat.recipient == get_main_profile(request):
-            recipient = chat.user
-            
+    else:
+        recipient = chat.recipient
+        
+    if request.method == 'POST':
+        message_form = MessageForm(request.POST)
+        
+        if message_form.is_valid():
+            message = Message()
+            message.chat = chat
+            message.sender_user = get_main_profile(request)
+            message.recipient_user = recipient
+            message.body = message_form.cleaned_data['body']
+            message.save()
+            return HttpResponseRedirect(reverse('chat', kwargs={'pk': pk}))
+        
         else:
-            recipient = chat.recipient
-            message_form = MessageForm(request.POST, request.FILES)
-            if message_form.is_valid():
-                message = Message()
-                message.chat = chat
-                message.sender_user = get_main_profile(request)
-                message.recipient_user = recipient
-                message.body = message_form.cleaned_data['body']
-                message.files = message_form.cleaned_data['files']
-                message.save()
+            context = {'form': message_form}
+            return render(request, 'chat/chat_detail.html', context)
+        
+    else:
+        message_form = MessageForm()
+        context = {'form': message_form}
+        return render(request, 'chat/chat_detail.html', context)
             
-            return redirect('chat', pk=pk)
-
-
-class ChatView(LoginRequiredMixin, View):
-    
-    login_url = reverse_lazy('login')
-    
-    def get(self, request, pk, *args, **kwargs):
         
-        form = MessageForm()
-        chat = Chat.objects.get(pk=pk)
-        message_list = Message.objects.filter(chat__pk__icontains=pk)
-        user = get_main_profile(request)
-        
-        context = {
+@login_required
+def chat_detail(request, pk):
+    user = get_main_profile(request)
+    chat = Chat.objects.get(pk=pk)
+    message_list = Message.objects.filter(chat__pk__icontains=pk)
+    form = MessageForm()
+    context = {
             'form': form,
             'chat': chat,
             'message_list': message_list,
-            'user': user
+            'user': user,
         }
-        
-        return render(request, 'chat/chat_detail.html', context=context)
-    
-    
+    return render(request, 'chat/chat_detail.html', context=context) 
