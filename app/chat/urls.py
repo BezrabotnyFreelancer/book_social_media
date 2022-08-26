4b582835e283d05b3e740aafec5c9d31538e8415
BezrabotnyFreelancer <ilya.vaitehovich@mail.ru>
2022-08-26 16:49:07 +0300

    create new urls

--- app/chat/urls.py
+++ app/chat/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('inbox/', views.list_chats, name='inbox'),
-    path('inbox/create-chat/', views.CreateChat.as_view(), name='create_chat'),
-    path('inbox/<int:pk>/', views.ChatView.as_view(), name='chat'),
-    path('inbox/<int:pk>/create-message/', views.CreateMessage.as_view(), name='create_message')
+    path('inbox/create-chat/', views.create_chat, name='create_chat'),
+    path('inbox/<int:pk>/', views.chat_detail, name='chat'),
+    path('inbox/<int:pk>/create-message/', views.create_message, name='create_message')
 ]
