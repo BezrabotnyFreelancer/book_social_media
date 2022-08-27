72f293c8b8fa895db7879dcd05ea943d2487146b
BezrabotnyFreelancer <ilya.vaitehovich@mail.ru>
2022-08-27 12:44:40 +0300

    Configure admin pannel

--- app/main/admin.py
+++ app/main/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin, TabularInline
 from .models import UserProfile, Book, Comment
 # Register your models here.
 
+class BookCommentInline(TabularInline):
+    model = Comment
+    
 
-admin.site.register(Book)
+@admin.register(Book)
+class BookAdmin(ModelAdmin):
+    list_display = ['author', 'genre', 'title']
+    list_filter = ['author']
+    inlines = [BookCommentInline]
+
+
 admin.site.register(UserProfile)
 admin.site.register(Comment)
