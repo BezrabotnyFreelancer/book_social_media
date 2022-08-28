51c3dfceaa23de6be69485e2c70130dd87929c1b
BezrabotnyFreelancer <ilya.vaitehovich@mail.ru>
2022-08-28 13:39:06 +0300

    add url of delete and update message

--- app/chat/urls.py
+++ app/chat/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('inbox/', views.list_chats, name='inbox'),
     path('inbox/create-chat/', views.create_chat, name='create_chat'),
     path('inbox/<uuid:pk>/', views.chat_detail, name='chat'),
-    path('inbox/<uuid:pk>/create-message/', views.create_message, name='create_message')
+    path('inbox/<uuid:pk>/create-message/', views.create_message, name='create_message'),
+    path('inbox/<uuid:pk>/edit-message/', views.MessageEdit.as_view(), name='message_edit'),
+    path('inbox/<uuid:pk>/delete-message/', views.MessageDelete.as_view(), name='message_delete')
 ]
