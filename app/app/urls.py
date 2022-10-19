c944be19bf5f7c267d79d4f8f3d9a23c47047ee7
BezrabotnyFreelancer <ilya.vaitehovich@mail.ru>
2022-10-19 16:20:44 +0300

    Add condition of settings debug

--- app/app/urls.py
+++ app/app/urls.py
@@ -15,12 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('api/v1/', include('api.urls')),
     path('', include('main.urls')),
     path('chat/', include('chat.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
