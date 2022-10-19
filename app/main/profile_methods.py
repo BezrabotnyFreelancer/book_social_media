3ba980e0795a51c9ae7dd50bfcfaa9472e4d2512
BezrabotnyFreelancer <ilya.vaitehovich@mail.ru>
2022-10-19 16:18:51 +0300

    Change filter >> get

--- app/main/profile_methods.py
+++ app/main/profile_methods.py
@@ -4,14 +4,16 @@ from django.shortcuts import get_object_or_404
 
 
 def get_main_profile(request):
-    user = get_user_model().objects.filter(username__icontains=request.user.get_username()).values('id')
-    profile = get_object_or_404(UserProfile, user=user[0]['id'])
+    user = get_user_model().objects.get(username__icontains=request.user.get_username())
+    profile = get_object_or_404(UserProfile, user=user)
     return profile
 
+
 def get_profile(username):
-    user = get_user_model().objects.filter(username__icontains=username).values('id')
-    profile = get_object_or_404(UserProfile, user=user[0]['id'])
+    user = get_user_model().objects.get(username__icontains=username)
+    profile = get_object_or_404(UserProfile, user=user)
     return profile
+
 
 def get_profile_by_id(id):
     profile = get_object_or_404(UserProfile, id=id)
